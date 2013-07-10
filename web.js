e5f10158210aee542c3098144a2322b0363c88f9
Ubuntu <ubuntu@ip-172-31-36-134.us-west-2.compute.internal>
2013-07-10 08:35:15 +0000

    Try to send indexhtml fle

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var fileData = new Buffer(fs.readFileSync("index.html"));
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  
+//  response.send('Hello World 2!');
+    response.send(fileData.toString());
 });
 
 var port = process.env.PORT || 5000;
