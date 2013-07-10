726c469b3791a7b2fe1519cf5426dd29c1e33bbf
Gustav Novotny <gustavnovotny@gmail.com>
2013-07-10 17:37:44 +0200

    grader - not final

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var urldata;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +58,35 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlUrl = function(htmlfile, checksfile) {
+    $ = cheerio.load(restler.get(htmlfile).on('complete', function(data) {
+        return data;
+    }));
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var getStringFromUrl = function(checksfile) {
+    
+    return restler.get('http://shrouded-bastion-8154.herokuapp.com/').on('complete', function(data) {
+//        console.log(data);
+        $ = cheerio.load(data);
+        var checks = loadChecks(checksfile).sort();
+        var out = {};
+        for(var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+        var outJson = JSON.stringify(out, null, 4);
+        console.log(outJson);
+    });
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +97,15 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL of the html structure')
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
+    
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    console.log("************************ ");
+    getStringFromUrl(program.checks);
+    
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
